ab591bd06a7e2b2847e318cd8845b200dc94e4a0
Maram-Khusheim <maram.khusheim@gmail.com>
2021-04-24 14:57:56 +0300

    First Commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        // Three ways to get button title
+        //print (sender.currentTitle!)
+        // print (sender.titleLabel?.text)
+        // print (sender.title(for: .normal))
+        
+        // Chose first methode
+        var senderTitle = sender.currentTitle!
+        
+        // Play sound function call with button title
+        playSound(soundName: senderTitle)
+        
+        // Set button opacity to half
+        sender.alpha = 0.5
+        
+        // Set timer seconds
+        let seconds = 0.2
+        // Create queue to delay for animation
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            // Return full opacity of button
+            sender.alpha = 1
+        }
+    }
 
+    func playSound(soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
 
 }
